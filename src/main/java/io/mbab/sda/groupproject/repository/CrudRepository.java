161f6208b160f93462f47b0ed64897463dde82d1
lukasz <lukaszsiembida@wp.pl>
2020-09-05 15:22:58 +0200

    AlbumRepository metody

--- src/main/java/io/mbab/sda/groupproject/repository/CrudRepository.java
+++ src/main/java/io/mbab/sda/groupproject/repository/CrudRepository.java
@@ -1,12 +1,13 @@
 package io.mbab.sda.groupproject.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CrudRepository<T, ID> {
 
   List<T> getAll();
 
-  T findById(ID id);
+  Optional<T> findById(ID id);
 
   T create(T entity);
 
